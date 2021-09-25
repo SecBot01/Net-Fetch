a590a873276323b21ea2919a26a77bcf9c38d862
muhammednahil <muhammednahilkiswa@gmail.com>
2021-09-25 18:41:16 +0530

    code formated

--- assets/netscan.py
+++ assets/netscan.py
@@ -25,7 +25,7 @@ def main():
     # gateway = str(subprocess.call("ip route show | grep -i 'default via'| awk '{print $3 }'", shell=True))
     # gateway_ip = str((gateway+"/24"))
     gateway_ip = raw_input("Enter your gateway ip with subnet (eg:192.168.1.1/24) \n\ntip: gateway ip can be obtained by running 'route -n' command\n\nip: ")
-    # gateway_ip = str(gateway_ip)
+    
     def scan(ip):
         arp_req = scapy.ARP(pdst=ip)
         broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
